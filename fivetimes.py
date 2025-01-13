3f65a6ae4af94c91ce6f8f181cf1a7be8e7e627d
paszosky <pavel.kowalczyk@gmail.com>
2025-01-13 12:10:33 +0100

    for loops

---
+++ fivetimes.py
@@ -0,0 +1,15 @@
+print('My name is')
+for i in range(5, 55, 5):
+    print('jimmy five times ' + str(i))
+
+print('My name is')
+for i in range(50, 0, -5):
+    print('jimmy five times ' + str(i))
+
+# gauss
+total = 0
+for i in range(101):
+    total = total + i
+
+print('The result is ' + str(total))
+
