03403accb28bcc1b0ed8956aa3004f4bc330cbba
paszosky <pavel.kowalczyk@gmail.com>
2025-01-20 14:53:49 +0100

    lists and strings

--- list_methods.py
+++ list_methods.py
@@ -1,3 +1,5 @@
+import copy
+
 spam = ['hello', 'hi', 'howdy', 'heyas']
 
 print(spam.index('howdy'))
@@ -22,3 +24,29 @@ animals.sort()
 print(animals)
 animals.sort(reverse=True)
 print(animals)
+
+spam = 42
+cheese = spam
+spam = 1000
+print(spam)
+print(cheese)
+
+spamL = [0, 1, 2, 3, 4, 5]
+cheeseL = spamL
+cheeseL[1] = 'hello'
+print(cheeseL)
+print(spamL)
+
+def eggs(someParameter):
+    someParameter.append('Helllo')
+
+spam = [1, 2, 3]
+eggs(spam)
+
+print(spam)
+
+spam3 = ['A', 'B', 'C', 'D']
+cheese3 = copy.deepcopy(spam3)
+cheese3[1] = 42
+print(cheese3)
+print(spam3)
