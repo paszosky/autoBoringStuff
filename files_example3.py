943291c1a142728ff84cbf7d9d5703fc7785b19b
paszosky <pavel.kowalczyk@gmail.com>
2025-01-25 23:56:36 +0100

    working with files 2

---
+++ files_example3.py
@@ -0,0 +1,9 @@
+import shutil
+# shutil.copy('hello3.txt', '..\\spam.txt')
+
+# shutil.copytree('.\\do-przeniesienia', '..\\przeniesiony-backup')
+
+# shutil.move('.\\do-przeniesienia\\hello3323.txt', '.\\')
+
+shutil.move('hello3323.txt', 'hello-new-name.txt')
+
