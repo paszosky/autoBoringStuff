13eb94538dfb7c2b963e6dc478d9fe194de13d4f
paszosky <pavel.kowalczyk@gmail.com>
2025-01-25 23:59:40 +0100

    working with files 3

--- files_example3.py
+++ files_example3.py
@@ -1,9 +1,13 @@
 import shutil
+import os
 # shutil.copy('hello3.txt', '..\\spam.txt')
 
 # shutil.copytree('.\\do-przeniesienia', '..\\przeniesiony-backup')
 
 # shutil.move('.\\do-przeniesienia\\hello3323.txt', '.\\')
 
-shutil.move('hello3323.txt', 'hello-new-name.txt')
+# shutil.move('hello3323.txt', 'hello-new-name.txt')
 
+# os.unlink('hello-new-name.txt')
+# os.rmdir('.\\do-przeniesienia')
+shutil.rmtree('.\\do-przeniesienia')
