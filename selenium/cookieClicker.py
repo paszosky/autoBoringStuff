83da33ac04a6ef25740d98f9690e12ac8c41841b
paszosky <pavel.kowalczyk@gmail.com>
2025-01-27 13:44:05 +0100

    cookie clicker

---
+++ selenium/cookieClicker.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+service = Service(executable_path="chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie_id = "bigCookie"
+#.fc-button-label
+
+
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "fc-button-label"))
+)
+
+cookiesAccept = driver.find_element(By.CLASS_NAME, "fc-button-label")
+cookiesAccept.click()
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'English')]"))
+)
+
+language = driver.find_element(By.XPATH, "//*[contains(text(), 'English')]")
+language.click()
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, cookie_id))
+)
+
+cookie = driver.find_element(By.ID, cookie_id)
+cookie.click()
+
+time.sleep(20)
